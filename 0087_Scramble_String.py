49a6a54f0de6f708fa0db8e228f0feb88629dbe2
yucongxing <yucongxing@163.com>
2021-04-16 20:30:45 +0800

    add problem 0087

---
+++ 0087_Scramble_String.py
@@ -0,0 +1,23 @@
+from typing import Counter
+from functools import lru_cache
+
+
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        @lru_cache(None)
+        def dfs(i1: int, i2: int, length: int) -> bool:
+            if s1[i1: i1+ length] == s2[i2: i2 + length]:
+                return True
+
+            if Counter(s1[i1: i1 + length]) != Counter(s2[i2: i2 + length]):
+                return False
+
+            for i in range(1, length):
+                if dfs(i1, i2, i) and dfs(i1 + i, i2 + i, length - i):
+                    return True
+                if dfs(i1, i2 + length - i, i) and dfs(i1 + i, i2, length - i):
+                    return True
+            return False
+        ans = dfs(0, 0, len(s1))
+        dfs.cache_clear()
+        return ans
