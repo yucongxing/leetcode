3b808c4722307d5afdb0ca79b37a62dce973bae9
yucongxing <yucongxing@163.com>
2021-06-05 23:38:11 +0800

    0203

---
+++ 0203_Remove_Linked_List_Elements.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        p = dummy
+        while p.next:
+            if p.next.val == val:
+                p.next = p.next.next
+            else:
+                p = p.next
+        return dummy.next
+
