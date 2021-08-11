4578926f38290d9946f736a87dd9e1b1d0f36f87
yucongxing <yucongxing@163.com>
2021-08-11 21:21:47 +0800

    add problem 0413

---
+++ 0413_Arithmetic_Slices.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 3:
+            return 0
+        dp = [0 for _ in range(n)]
+        ans = 0
+        for i in range(2,n):
+            if nums[i] - nums[i-1] == nums[i - 1] - nums[i - 2]:
+                dp[i] = dp[i - 1] + 1
+                ans += dp[i]
+        return ans
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.numberOfArithmeticSlices([1,2,3,4]))
+
