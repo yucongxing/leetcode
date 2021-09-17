c7530d6a316efdda308c47c0d221dc5061d3b991
yucongxing <yucongxing@outlook.com>
2021-09-17 17:36:15 +0800

    optimize 0036

--- 0036_Valid_Sudoku.cpp
+++ 0036_Valid_Sudoku.cpp
@@ -8,43 +8,19 @@ typedef long long ll;
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<int> inum(9, 0), jnum(9, 0), snum(9, 0);
+        vector<int> inum(9, 0), jnum(9, 0);
+        vector<vector<int>> snum(3, vector<int>(3, 0));
         for (int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board[0].size(); j++){
-                cout << board[i][j] << endl;
+                if (board[i][j] == '.') continue;
+
                 int x = board[i][j] -'0';
-                if (inum[i] & (1 << x)) return false;
-                else inum[i] ^= (1 << x);
-                if (jnum[j] & (1 << x)) return false;
-                else jnum[j] ^= (1 << x);
-                if(i / 3 == 0 && j / 3 == 0) {
-                    if (snum[0] & (1 << x)) return false;
-                    else snum[0] ^= (1 << x);
-                }else if(i / 3 == 1 && j / 3 == 0) {
-                    if (snum[1] & (1 << x)) return false;
-                    else snum[1] ^= (1 << x);
-                }else if(i / 3 == 2 && j / 3 == 0) {
-                    if (snum[2] & (1 << x)) return false;
-                    else snum[2] ^= (1 << x);
-                }else if(i / 3 == 0 && j / 3 == 1) {
-                    if (snum[3] & (1 << x)) return false;
-                    else snum[3] ^= (1 << x);
-                }else if(i / 3 == 1 && j / 3 == 1) {
-                    if (snum[4] & (1 << x)) return false;
-                    else snum[4] ^= (1 << x);
-                }else if(i / 3 == 2 && j / 3 == 1) {
-                    if (snum[5] & (1 << x)) return false;
-                    else snum[5] ^= (1 << x);
-                }else if(i / 3 == 0 && j / 3 == 2) {
-                    if (snum[6] & (1 << x)) return false;
-                    else snum[6] ^= (1 << x);
-                }else if(i / 3 == 1 && j / 3 == 2) {
-                    if (snum[7] & (1 << x)) return false;
-                    else snum[7] ^= (1 << x);
-                }else{
-                    if (snum[8] & (1 << x)) return false;
-                    else snum[8] ^= (1 << x);
-                }
+
+                if (inum[i] & (1 << x) || jnum[j] & (1 << x) && snum[i / 3][j / 3] & (1 << x)) return false;
+
+                inum[i] ^= (1 << x);
+                jnum[j] ^= (1 << x);
+                snum[i / 3][j / 3] ^= (1 << x);
             }
         }
         return true;
