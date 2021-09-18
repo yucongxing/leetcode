8fb4a22b5f1479f22dc92f2134ee39b927bf5e1a
yucongxing <yucongxing@outlook.com>
2021-09-18 10:48:54 +0800

    add problem 0292

---
+++ 0292_Nim_Game.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+typedef long long ll;
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n % 4;
+    }
+};
+
+int main() {
+    Solution s;
+    return 0;
+}
