6f7090469db7bed791739e38d4a4acc8bc9dc784
yucongxing <yucongxing@163.com>
2021-10-10 00:38:33 +0800

    add problem 0352

---
+++ 0352_Data_Stream_as_Disjoint_intervals.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+using namespace std;
+typedef long long ll;
+
+class SummaryRanges {
+private:
+    map<int, int> intervals;
+
+public: SummaryRanges() {
+    }
+
+    void addNum(int val) {
+        auto interval1 = intervals.upper_bound(val);
+        auto interval0 = (interval1 == intervals.begin() ? intervals.end() : prev(interval1));
+
+        if (interval0 != intervals.end() && interval0->first <= val && val <= interval0->second) {
+            return;
+        }
+        else {
+            bool left_aside = (interval0 != intervals.end() && interval0->second + 1 == val);
+            bool right_aside = (interval1 != intervals.end() && interval1->first - 1 == val);
+            if (left_aside && right_aside) {
+                int left = interval0->first, right = interval1->second;
+                intervals.erase(interval0);
+                intervals.erase(interval1);
+                intervals.emplace(left, right);
+            }else if (left_aside) {
+                ++interval0->second;
+            }else if (right_aside) {
+                int right = interval1->second;
+                intervals.erase(interval1);
+                intervals.emplace(val, right);
+            }else {
+                intervals.emplace(val, val);
+            }
+        }
+    }
+
+    vector<vector<int>> getIntervals() {
+        vector<vector<int>> ans;
+        for (const auto& [left, right] : intervals) {
+            ans.push_back({left, right});
+        }
+        return ans;
+    }
+};
+
+int main() {
+    SummaryRanges s;
+    return 0;
+}
